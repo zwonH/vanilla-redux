3c3caf65c568ebbc5c682aa9a1e8d68c61508c7f
zwonH <a.circle.one@gmail.com>
2020-03-09 16:40:43 +0900

    ToDos using redux

--- src/index.js
+++ src/index.js
@@ -1,53 +1,76 @@
 import { createStore } from 'redux';
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
 
-number.innerText = 0;
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
 
-const ADD = 'ADD';
-const MINUS = 'MINUS';
-//이렇게 입력해놓고 스트링 대신 사용해주면 오류를 좀 더 빠르게 찾는데 용이함
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
 
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
+  console.log(action);
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
+    case DELETE_TODO:
+      const cleaned = state.filter(toDo => toDo.id !== action.id);
+      return cleaned;
     default:
-      return count;
+      return state;
   }
 };
-//reducer: function that modifies the data
-//이 함수가 return하는 것이 바로 앱의 data가 되는 것
-//유일하게 data를 modity할 수 있는 곳
-//파라 미터 안에 initial 값을 지정해줄 수 있음
 
-const countStore = createStore(countModifier);
-//store: data를 넣는 곳
-//redux의 createStore 함수를 사용해서 store 생성
-//괄호 안에는 어떤 함수를 사용해서 이 data를 modify 해줄 지
+const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+store.subscribe(() => console.log(store.getState()));
+
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
 };
 
-countStore.subscribe(onChange);
-//subscribe: allows us to listen for changes in the store
-//store에 변화가 감지될 때마다 불려짐
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
+const dispatchDeleteToDo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach(toDo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'DEL';
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
 };
 
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
-//dispatch를 사용해서 일종의 메세지를 보내는 것
-//메세지에는 action을 넣게 되고, 리듀서 함수 내에서 action.type을 확인하는거지
+store.subscribe(paintToDos);
 
-// console.log(countStore.getState());
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  dispatchAddToDo(toDo);
+};
+
+form.addEventListener('submit', onSubmit);
+
+//state is read-only
+// ** return new state objects! Never mutate the previous state!
